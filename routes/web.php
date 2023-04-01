587f84f0a6bbaab024762222014bc98d834aaf10
Oguzhan Karahan <oguzhankm@gmail.com>
2023-04-01 20:44:48 +0300

    Controller kısmında tablo ekleme kısmı bitti

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function (){
+    Route::get('/', [\App\Http\Controllers\Admin\AdminController::class, 'index'])->name('index');
+    Route::resource('/categories', \App\Http\Controllers\Admin\CategoryController::class);
+    Route::resource('/menus', \App\Http\Controllers\Admin\MenuController::class);
+});
+
+require __DIR__.'/auth.php';
